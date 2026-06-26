e4c5f78841b78014e1e3ada0a7b8bb3fc16a126f
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2026-06-26 07:48:37 +0200

    Stub GridLayout and apply to index route
    #6

--- apps/cockpit/src/routes/index.tsx
+++ apps/cockpit/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { WeatherWidget } from '@/domain/weather/WeatherWidget.tsx';
 import { LOCATION_MOESSINGEN, LOCATION_OBERNHEIM } from '@/domain/weather/model/model.ts';
+import { GridLayout } from '@/components/ContentLayout/GridLayout.tsx';
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -8,12 +9,12 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <>
+    <GridLayout>
       <WeatherWidget location={LOCATION_MOESSINGEN} />
       <WeatherWidget location={LOCATION_OBERNHEIM} />
       {/*
       <VoiceWidget />
       */}
-    </>
+    </GridLayout>
   );
 }
